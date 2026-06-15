0fe0ac6602aad53017ad1db7dc47b994cc7dbd75
Douglas Jasper <doug.jasper@trios.com>
2026-06-15 08:23:30 -0400

    Added TimeManager Class

---
+++ WorkoutTimer2026 Watch App/TimerManager.swift
@@ -0,0 +1,18 @@
+//
+//  TimerManager.swift
+//  WorkoutTimer2026 Watch App
+//
+//  Created by Douglas Jasper on 2026-06-15.
+//
+
+import Foundation
+import Combine
+import UserNotifications
+import WatchKit
+
+@MainActor
+final class TimerManager: ObservableObject {
+    
+}
+
+
