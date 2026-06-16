964ae842209997a674552f526198f36f79118aca
Douglas Jasper <doug.jasper@trios.com>
2026-06-16 08:22:17 -0400

    ContentView variables created

--- WorkoutTimer2026 Watch App/ContentView.swift
+++ WorkoutTimer2026 Watch App/ContentView.swift
@@ -10,6 +10,26 @@ import UserNotifications
 import WatchKit
 
 struct ContentView: View {
+    
+    @StateObject private var timerManager = TimerManager()
+    private let lastPresetKey = "lastSelectedPresetIndex"
+    private let lastCustomTimeKey = "lastCustomTimeSeconds"
+    
+    // Presets
+    let presets: [TimeInterval] = [60, 5*60, 10*60, 20*60, 30*60]
+    let presetLabels: [String] = ["1m", "5m", "10m", "20m", "30m"]
+    
+    // track selected preset index
+    @State private var selectedPresetIndex: Int = {
+        let saved = UserDefaults.standard.object(forKey: "lastSelectedPresetIndex") as? Int
+        return saved ?? 1
+    }()
+    
+    // Custom timer
+    @State private var ShowingCustomTimeSheet = false
+    @State private var customMinutes: Int = 1
+    @State private var customSeconds: Int = 0
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
